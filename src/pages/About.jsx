b571212b9883dca6eab129401828d33decfd8b1b
hashzaidi25 <151329967+hashzaidi25@users.noreply.github.com>
2023-11-18 11:30:55 +0530

    Update About.jsx

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -22,8 +22,7 @@ const About = () => {
 
       <div className='mt-5 flex flex-col gap-3 text-slate-500'>
         <p>
-          Software Engineer based in Croatia, specializing in technical
-          education through hands-on learning and building applications.
+          a passionate software engineer, specializes in crafting dynamic and innovative web applications. With a keen eye for design and a love for clean code.
         </p>
       </div>
 
